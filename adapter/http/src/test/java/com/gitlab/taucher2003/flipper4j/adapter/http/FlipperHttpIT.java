29581f007e09d09bb9216b786cfcff891e4c2570
Niklas van Schrick <mc.taucher2003@gmail.com>
2022-09-20 15:58:23 +0200

    Make integration test abstract

--- adapter/http/src/test/java/com/gitlab/taucher2003/flipper4j/adapter/http/FlipperHttpIT.java
+++ adapter/http/src/test/java/com/gitlab/taucher2003/flipper4j/adapter/http/FlipperHttpIT.java
@@ -2,29 +2,15 @@ package com.gitlab.taucher2003.flipper4j.adapter.http;
 
 import com.gitlab.taucher2003.flipper4j.adapter.http.config.FlipperConfigurator;
 import com.gitlab.taucher2003.flipper4j.core.Flipper;
-import com.gitlab.taucher2003.flipper4j.core.model.FlipperIdentifier;
-import org.assertj.core.data.Offset;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.gitlab.taucher2003.flipper4j.core.FlipperIT;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
-
-@Testcontainers
-class FlipperHttpIT {
+public class FlipperHttpIT extends FlipperIT {
 
     public static final String DEFAULT_BACKEND_IMAGE = "registry.gitlab.com/taucher2003-group/flipper4j/test-backend:" + System.getenv().getOrDefault("T_TEST_BACKEND_IMAGE_TAG", "latest");
 
@@ -39,152 +25,11 @@ class FlipperHttpIT {
             .withStartupTimeout(Duration.of(500, ChronoUnit.SECONDS))
             .withStartupAttempts(3);
 
-    public Flipper flipper;
-
-    @BeforeEach
-    void setUp() {
+    @Override
+    protected Flipper createFlipper(boolean shouldWork) {
         var adapter = new FlipperConfigurator()
-                .setBaseUrl("http://" + flipperApi.getHost() + ":" + flipperApi.getFirstMappedPort())
+                .setBaseUrl("http://" + flipperApi.getHost() + ":" + (flipperApi.getFirstMappedPort() + (shouldWork ? 0 : 1)))
                 .build();
-        flipper = Flipper.create(adapter);
-        flipper.awaitReady(5, TimeUnit.SECONDS);
-    }
-
-    @AfterEach
-    void tearDown() {
-        flipper.shutdown();
-    }
-
-    @Test
-    void awaitReady() {
-        var adapter = new FlipperConfigurator()
-                .setBaseUrl("http://" + flipperApi.getHost() + ":" + (flipperApi.getFirstMappedPort() + 1))
-                .build();
-        var flipper = Flipper.create(adapter);
-        var waitStart = System.currentTimeMillis();
-        flipper.awaitReady(1, TimeUnit.SECONDS);
-        var waitEnd = System.currentTimeMillis();
-
-        flipper.shutdown();
-
-        assertThat(waitEnd - waitStart).isCloseTo(TimeUnit.SECONDS.toMillis(1), Offset.offset(100L));
-    }
-
-    @Test
-    void deleteFeature() {
-        flipper.admin().createFeature("test_flag").join();
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-
-        flipper.admin().deleteFeature("test_flag").join();
-
-        assertThat(flipper.getFeature("test_flag")).isEmpty();
-    }
-
-    @Test
-    void clearFeature() {
-        flipper.admin().createFeature("test_flag").join();
-        flipper.admin().enableFeatureBoolean("test_flag").join();
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag")).isTrue();
-
-        flipper.admin().clearFeature("test_flag").join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag")).isFalse();
-    }
-
-    @Test
-    void disabledFlagWithoutActor() {
-        flipper.admin().createFeature("test_flag").join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag")).isFalse();
-    }
-
-    @Test
-    void disabledFlagWithActor() {
-        FlipperIdentifier flipperIdentifier = () -> "actor";
-
-        flipper.admin().createFeature("test_flag").join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag", flipperIdentifier)).isFalse();
-    }
-
-    @Test
-    void nonExistentFlag() {
-        assertThat(flipper.getFeature("non_existent")).isEmpty();
-        assertThat(flipper.isEnabled("non_existent")).isFalse();
-    }
-
-    @Test
-    void booleanEnabledFlagWithoutActor() {
-        flipper.admin().createFeature("test_flag").join();
-        flipper.admin().enableFeatureBoolean("test_flag").join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag")).isTrue();
-    }
-
-    @Test
-    void actorEnabledFlagWithoutActor() {
-        FlipperIdentifier actor = () -> "actor";
-
-        flipper.admin().createFeature("test_flag").join();
-        flipper.admin().enableFeatureActor("test_flag", actor).join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag")).isFalse();
-    }
-
-    @Test
-    void actorEnabledFlagWithActor() {
-        FlipperIdentifier actor = () -> "actor";
-
-        flipper.admin().createFeature("test_flag").join();
-        flipper.admin().enableFeatureActor("test_flag", actor).join();
-
-        assertThat(flipper.getFeature("test_flag")).isPresent();
-        assertThat(flipper.isEnabled("test_flag", actor)).isTrue();
-    }
-
-    @Test
-    void percentageActorEnabledWithActor() {
-        FlipperIdentifier actor = () -> "actor:5";
-
-        flipper.admin().createFeature("feature").join();
-        flipper.admin().enableFeatureActorPercentage("feature", 50).join();
-
-        assertThat(flipper.getFeature("feature")).isPresent();
-        assertThat(flipper.isEnabled("feature", actor)).isTrue();
-    }
-
-    @Test
-    void percentageActorEnabledWithWrongActor() {
-        FlipperIdentifier actor = () -> "actor:4";
-
-        flipper.admin().createFeature("feature").join();
-        flipper.admin().enableFeatureActorPercentage("feature", 50).join();
-
-        assertThat(flipper.getFeature("feature")).isPresent();
-        assertThat(flipper.isEnabled("feature", actor)).isFalse();
-    }
-
-    @Test
-    void percentageTimeEnabled() {
-        flipper.admin().createFeature("test_flag").join();
-        flipper.admin().enableFeatureTimePercentage("test_flag", 50).join();
-
-        try(var mockStatic = mockStatic(ThreadLocalRandom.class)) {
-            var random = mock(ThreadLocalRandom.class);
-            when(random.nextInt(anyInt()))
-                    .thenReturn(49)
-                    .thenReturn(50);
-
-            when(ThreadLocalRandom.current()).thenReturn(random);
-
-            assertThat(flipper.isEnabled("test_flag")).isTrue();
-            assertThat(flipper.isEnabled("test_flag")).isFalse();
-        }
+        return flipper = Flipper.create(adapter);
     }
 }
---
+++ core/src/test/java/com/gitlab/taucher2003/flipper4j/core/FlipperIT.java
@@ -0,0 +1,170 @@
+package com.gitlab.taucher2003.flipper4j.core;
+
+import com.gitlab.taucher2003.flipper4j.core.model.FlipperIdentifier;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@Testcontainers
+public abstract class FlipperIT {
+
+    public Flipper flipper;
+
+    protected Flipper createFlipper() {
+        return createFlipper(true);
+    }
+
+    protected abstract Flipper createFlipper(boolean shouldWork);
+
+    @BeforeEach
+    void setUp() {
+        flipper = createFlipper();
+        flipper.awaitReady(5, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        flipper.shutdown();
+    }
+
+    @Test
+    void awaitReady() {
+        var flipper = createFlipper(false);
+        var waitStart = System.currentTimeMillis();
+        flipper.awaitReady(1, TimeUnit.SECONDS);
+        var waitEnd = System.currentTimeMillis();
+
+        flipper.shutdown();
+
+        assertThat(waitEnd - waitStart).isCloseTo(TimeUnit.SECONDS.toMillis(1), Offset.offset(100L));
+    }
+
+    @Test
+    void deleteFeature() {
+        flipper.admin().createFeature("test_flag").join();
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+
+        flipper.admin().deleteFeature("test_flag").join();
+
+        assertThat(flipper.getFeature("test_flag")).isEmpty();
+    }
+
+    @Test
+    void clearFeature() {
+        flipper.admin().createFeature("test_flag").join();
+        flipper.admin().enableFeatureBoolean("test_flag").join();
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag")).isTrue();
+
+        flipper.admin().clearFeature("test_flag").join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag")).isFalse();
+    }
+
+    @Test
+    void disabledFlagWithoutActor() {
+        flipper.admin().createFeature("test_flag").join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag")).isFalse();
+    }
+
+    @Test
+    void disabledFlagWithActor() {
+        FlipperIdentifier flipperIdentifier = () -> "actor";
+
+        flipper.admin().createFeature("test_flag").join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag", flipperIdentifier)).isFalse();
+    }
+
+    @Test
+    void nonExistentFlag() {
+        assertThat(flipper.getFeature("non_existent")).isEmpty();
+        assertThat(flipper.isEnabled("non_existent")).isFalse();
+    }
+
+    @Test
+    void booleanEnabledFlagWithoutActor() {
+        flipper.admin().createFeature("test_flag").join();
+        flipper.admin().enableFeatureBoolean("test_flag").join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag")).isTrue();
+    }
+
+    @Test
+    void actorEnabledFlagWithoutActor() {
+        FlipperIdentifier actor = () -> "actor";
+
+        flipper.admin().createFeature("test_flag").join();
+        flipper.admin().enableFeatureActor("test_flag", actor).join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag")).isFalse();
+    }
+
+    @Test
+    void actorEnabledFlagWithActor() {
+        FlipperIdentifier actor = () -> "actor";
+
+        flipper.admin().createFeature("test_flag").join();
+        flipper.admin().enableFeatureActor("test_flag", actor).join();
+
+        assertThat(flipper.getFeature("test_flag")).isPresent();
+        assertThat(flipper.isEnabled("test_flag", actor)).isTrue();
+    }
+
+    @Test
+    void percentageActorEnabledWithActor() {
+        FlipperIdentifier actor = () -> "actor:5";
+
+        flipper.admin().createFeature("feature").join();
+        flipper.admin().enableFeatureActorPercentage("feature", 50).join();
+
+        assertThat(flipper.getFeature("feature")).isPresent();
+        assertThat(flipper.isEnabled("feature", actor)).isTrue();
+    }
+
+    @Test
+    void percentageActorEnabledWithWrongActor() {
+        FlipperIdentifier actor = () -> "actor:4";
+
+        flipper.admin().createFeature("feature").join();
+        flipper.admin().enableFeatureActorPercentage("feature", 50).join();
+
+        assertThat(flipper.getFeature("feature")).isPresent();
+        assertThat(flipper.isEnabled("feature", actor)).isFalse();
+    }
+
+    @Test
+    void percentageTimeEnabled() {
+        flipper.admin().createFeature("test_flag").join();
+        flipper.admin().enableFeatureTimePercentage("test_flag", 50).join();
+
+        try(var mockStatic = mockStatic(ThreadLocalRandom.class)) {
+            var random = mock(ThreadLocalRandom.class);
+            when(random.nextInt(anyInt()))
+                    .thenReturn(49)
+                    .thenReturn(50);
+
+            when(ThreadLocalRandom.current()).thenReturn(random);
+
+            assertThat(flipper.isEnabled("test_flag")).isTrue();
+            assertThat(flipper.isEnabled("test_flag")).isFalse();
+        }
+    }
+}
